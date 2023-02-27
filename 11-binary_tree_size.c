8c78362b835eb3544ccb5bc8d3f89940cee2ab23
Nico <lawani.gk@gmail.com>
2023-02-27 20:41:44 +0100

    add function to compute tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,12 +8,11 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t left_h, right_h;
-		if (tree->left)
-			left_h = 1 + binary_tree_size(tree->left);
-		if (tree->right)
-			right_h = 1 + binary_tree_size(tree->right);
-		return (left_h + right_h)
+		size_t left_h = 0, right_h = 0;
+
+		left_h = binary_tree_size(tree->left);
+		right_h = binary_tree_size(tree->right);
+		return (left_h + right_h + 1);
 	}
 	return (0);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			left_height = 1 + binary_tree_height(tree->left);
 		if (tree->right)
 			right_height = 1 + binary_tree_height(tree->right);
-		if (left_height > right_height)
+		if (left_height >= right_height)
 			return left_height;
 		else
 			return right_height;

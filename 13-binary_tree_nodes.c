4bfeddff27fc1bcc2116c1e94f4cbeac2a39a676
Nico <lawani.gk@gmail.com>
2023-02-28 10:55:01 +0100

    refactor code

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -30,6 +30,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 			left = binary_tree_nodes(tree->left);
 		if (tree->right)
 			right = binary_tree_nodes(tree->right);
+		if (tree->left || tree->right)
+			return (1 + left + right);
 	}
-	return (!binary_tree_is_leaf(tree) + left + right);
+	return (0);
 }

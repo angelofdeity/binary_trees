7e4f5ee048d915fd1ff6f722694e07ff74bbb610
Nico <lawani.gk@gmail.com>
2023-02-28 18:25:05 +0100

    modify file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -79,5 +79,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	return (!binary_tree_balance(tree) * binary_tree_is_full(tree));
+	return (!binary_tree_balance(tree) && binary_tree_is_full(tree));
 }

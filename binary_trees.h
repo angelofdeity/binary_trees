502ef7cb343dd219de825abe4af6fdc0b6e3a30c
Nico <lawani.gk@gmail.com>
2023-02-28 09:03:09 +0100

    modify header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
 #include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,13 +10,14 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
@@ -37,4 +37,4 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 
-#endif /* BINARY_TREES_H */
+#endif
